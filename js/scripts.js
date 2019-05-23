6b2053754cbff32511ffb3b2ec61408ce167bc37
MariyaKapiturko <mariya.kapiturko@mail.ru>
2019-05-23 21:28:02 +0300

    animation and menu

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+new WOW().init();
+
+$(document).ready(function() {
+    var $toggleButton = $('.toggle-button'),
+        $menuWrap = $('.menu-wrap');
+    $toggleButton.on('click', function() {
+        $(this).toggleClass('button-open');
+        $menuWrap.toggleClass('menu-show');
+    });
+});
